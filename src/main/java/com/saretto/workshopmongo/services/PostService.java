051a08a0ec07b99ecacaf8a525de77dd86b95fa0
GabrielSaretto <gabriel.saretto2@gmail.com>
2024-01-30 08:22:38 -0300

    Obtendo um post por id

---
+++ src/main/java/com/saretto/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.saretto.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.saretto.workshopmongo.domain.Post;
+import com.saretto.workshopmongo.repository.PostRepository;
+import com.saretto.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
