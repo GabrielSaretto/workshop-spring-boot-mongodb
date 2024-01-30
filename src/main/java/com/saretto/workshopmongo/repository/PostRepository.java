0eb540d45229f384c431fabd197fdcda57bc649d
GabrielSaretto <gabriel.saretto2@gmail.com>
2024-01-30 07:37:22 -0300

    Criando entity Post com user aninhado

---
+++ src/main/java/com/saretto/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.saretto.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.saretto.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+	
+}
